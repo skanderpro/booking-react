c343063b041977706091f754b15e475b3e03787e
kiptykDima <kiptykdmytro@gmail.com>
2022-07-26 12:47:16 +0300

    update header and footer

---
+++ src/components/Header/HeaderLinks.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function HeaderLinks() {
+    return <div className={'social-links-header-links'}>
+        <div className='container'>
+				<div className='header-links'>
+					<a href={''}>New fabric online now!</a>
+					<a href={''}>Shop Sewing Machines</a>
+					<a href={''}>FREE shipping on all orders over £75</a>
+				</div>
+		  </div>
+    </div>
+
+}
+export default HeaderLinks;
