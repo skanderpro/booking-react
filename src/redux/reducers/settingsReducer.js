1ce90c18ba442f4d78e8bd43c465b95bc8ecdae8
Bryniuk Valia <valyanychyporuk@gmail.com>
2022-10-26 14:16:56 +0300

    change url

--- src/redux/reducers/settingsReducer.js
+++ src/redux/reducers/settingsReducer.js
@@ -1,8 +1,8 @@
 import { INIT_SETTINGS } from "../actions/actionTypes";
 
 const initializeState = {
-  // mainUrl: "https://crm.sewconfident.co.uk",
-  mainUrl: "http://127.0.0.1:8000",
+  mainUrl: "https://crm.sewconfident.co.uk",
+  // mainUrl: "http://127.0.0.1:8000",
 };
 
 export default function settingsReducer(state = initializeState, action) {
