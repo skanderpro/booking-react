fd3f38cca5333ec43a5d961a525bc4727622de6b
kiptykDima <kiptykdmytro@gmail.com>
2023-01-27 20:26:16 +0200

    update style

--- src/components/Footer/MailContainer.js
+++ src/components/Footer/MailContainer.js
@@ -1,72 +1,76 @@
 import React, { useState, useEffect } from "react";
-import { InputGroup, FormControl } from "react-bootstrap";
+import { InputGroup, FormControl, Form } from "react-bootstrap";
 import { connect } from "react-redux";
 import { putEmailToGroup } from "./../../redux/actions/mailerliteActions";
 import { NotificationManager } from "react-notifications";
 
 function MailContainer(props) {
-  useEffect(() => {}, []);
-  let [email, setEmail] = useState("");
+	useEffect(() => { }, []);
+	let [email, setEmail] = useState("");
 
-  return (
-    <div className={"mail-container"}>
-      <div className={"row no-gutters"}>
-        <div className={" text-block"}>
-          <h2>Join Our Mailing List</h2>
-          <p>
-            Be the first to know about exclusive shop discounts, product
-            restocks and new launches. Sign up to the Sew Confident mailing
-            list!
-          </p>
-        </div>
-        <div className={" form-block"}>
-          <InputGroup>
-            <FormControl
-              placeholder="Your email"
-              aria-label="Recipient's username"
-              aria-describedby="basic-addon2"
-              value={email}
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-            />
-            <InputGroup.Append>
-              <button
-                className={"btn"}
-                onClick={() => {
-                  props
-                    .putEmailToGroup(email)
-                    .then((response) => {
-                      if (!response.data.hasOwnProperty("error")) {
-                        NotificationManager.success("Subscribe success");
-                      } else {
-                        NotificationManager.error(response.data.error.message);
-                      }
+	return (
+		<div className={"mail-container"}>
+			<div className={"row no-gutters"}>
+				<div className={" text-block"}>
+					<h2>Join Our Mailing List</h2>
+					<p>
+						Get all updates directly to your inbox
+					</p>
+				</div>
+				<div className={"form-block"}>
+					<InputGroup>
+						<FormControl
+							placeholder="Your email"
+							aria-label="Recipient's username"
+							aria-describedby="basic-addon2"
+							value={email}
+							onChange={(e) => {
+								setEmail(e.target.value);
+							}}
+						/>
+						<InputGroup.Append>
+							<button
+								className={"btn"}
+								onClick={() => {
+									props
+										.putEmailToGroup(email)
+										.then((response) => {
+											if (!response.data.hasOwnProperty("error")) {
+												NotificationManager.success("Subscribe success");
+											} else {
+												NotificationManager.error(response.data.error.message);
+											}
 
-                      setEmail("");
-                    })
-                    .catch((errors) => {
-                      NotificationManager.error("Some error");
-                    });
-                }}
-              >
-                Subscribe
-              </button>
-            </InputGroup.Append>
-          </InputGroup>
-        </div>
-      </div>
-    </div>
-  );
+											setEmail("");
+										})
+										.catch((errors) => {
+											NotificationManager.error("Some error");
+										});
+								}}
+							>
+								Subscribe
+							</button>
+						</InputGroup.Append>
+					</InputGroup>
+					<FormControl as="select" className={"select-footer"} aria-label="Default select example">
+						<option>Open this select menu</option>
+						<option value="1">One</option>
+						<option value="2">Two</option>
+						<option value="3">Three</option>
+					</FormControl>
+				</div>
+			</div>
+		</div>
+	);
 }
 function mapStateToProps(state) {
-  return {};
+	return {};
 }
 function mapDispatchToProps(dispatch) {
-  return {
-    putEmailToGroup: (email) => {
-      return dispatch(putEmailToGroup(email));
-    },
-  };
+	return {
+		putEmailToGroup: (email) => {
+			return dispatch(putEmailToGroup(email));
+		},
+	};
 }
 export default connect(mapStateToProps, mapDispatchToProps)(MailContainer);
--- src/components/components/Calendar.js
+++ src/components/components/Calendar.js
@@ -1,78 +1,79 @@
-import React, {useMemo} from "react";
-import FullCalendar from '@fullcalendar/react' // must go before plugins
-import {NavLink, withRouter} from "react-router-dom";
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
-import {makeUrl} from "../../redux/actions/functions";
-import {connect} from "react-redux";
+import React, { useMemo } from "react";
+import FullCalendar from '@fullcalendar/react'
+import { NavLink, withRouter } from "react-router-dom";
+import dayGridPlugin from '@fullcalendar/daygrid'
+
+import { makeUrl } from "../../redux/actions/functions";
+import { connect } from "react-redux";
 
 function Calendar(props) {
 
-    console.log(props.settings);
+	console.log(props.settings);
 
 
-    const events = useMemo(() => {
-        return props.items.map(event => {
-            const isCourse = event.type === 'course';
-            const date = new Date();
-            const dateParts = event.product.lesson_date.split('/');
-            date.setFullYear(dateParts[2]);
-            date.setMonth(+dateParts[1] - 1);
-            date.setDate(dateParts[0]);
+	const events = useMemo(() => {
+		return props.items.map(event => {
+			const isCourse = event.type === 'course';
+			const date = new Date();
+			const dateParts = event.product.lesson_date.split('/');
+			date.setFullYear(dateParts[2]);
+			date.setMonth(+dateParts[1] - 1);
+			date.setDate(dateParts[0]);
 
-            //   console.log('date', dateParts.join('/'), date);
+			//   console.log('date', dateParts.join('/'), date);
 
-            return {
-                title: event.product.name,
-                date,
-                imageurl: props.settings.mainUrl + '/storage/' + event.product.image_url,
-                extendedProps: event
-            }
-        });
-    }, [props.items]);
+			return {
+				title: event.product.name,
+				date,
+				imageurl: props.settings.mainUrl + '/storage/' + event.product.image_url,
+				extendedProps: event
+			}
+		});
+	}, [props.items]);
 
 
-    function renderEventContent(eventInfo) {
-        console.log('eventInfo', eventInfo);
-        const linkParam = eventInfo.event.extendedProps.product.slug || eventInfo.event.extendedProps.product.id;
+	function renderEventContent(eventInfo) {
+		console.log('eventInfo', eventInfo);
+		const linkParam = eventInfo.event.extendedProps.product.slug || eventInfo.event.extendedProps.product.id;
 
-        return (
-            <a className="calendar-class"
-                     href={`/class-detail/${linkParam}`}>
-                <div
-                    className="calendar-class-title">{eventInfo.event.title}</div>
-                <div className="calendar-class-box">
-                    <div className="calendar-class-inner">
-                        <div
-                            className="calendar-class-date">{eventInfo.timeText}</div>
-                        <div
-                            className="calendar-class-price">£{eventInfo.event.extendedProps.product.price}</div>
-                    </div>
+		return (
+			<a className="calendar-class"
+				href={`/class-detail/${linkParam}`}>
+				<div
+					className="calendar-class-title">{eventInfo.event.title}</div>
+				<div className="calendar-class-box">
+					<div className="calendar-class-inner">
+						<div
+							className="calendar-class-date">{eventInfo.timeText}</div>
+						<div
+							className="calendar-class-price">£{eventInfo.event.extendedProps.product.price}</div>
+					</div>
 
-                    <div className="calendar-class-img">
-                        <img src={eventInfo.event.extendedProps.imageurl}
-                             alt={eventInfo.event.title}/>
-                    </div>
-                </div>
-            </a>
-        )
-    }
+					<div className="calendar-class-img">
+						<img src={eventInfo.event.extendedProps.imageurl}
+							alt={eventInfo.event.title} />
+					</div>
+				</div>
+			</a>
+		)
+	}
 
-    return (
-        
-                <FullCalendar
-                    plugins={[dayGridPlugin]}
-                    initialView="dayGridMonth"
-                    eventContent={renderEventContent}
-                    events={events}
-                />
-           
-    );
+	return (
+
+		<FullCalendar
+			plugins={[dayGridPlugin]}
+			initialView="dayGridMonth"
+			eventContent={renderEventContent}
+			events={events}
+		/>
+
+	);
 }
 
 function mapStateToProps(state) {
-    return {
-        settings: state.settings,
-    };
+	return {
+		settings: state.settings,
+	};
 }
 
 export default connect(mapStateToProps)(withRouter(Calendar));
